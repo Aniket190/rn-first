b5b53514a2ee3df411c30a1230fab4cd9a78d33c
Aniket <aniketkumar190@gmail.com>
2021-03-08 11:25:37 +0530

    Counter Screen Working (useStatae)

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React,{useState} from "react";
 import {Text,View,StyleSheet,Button} from "react-native"
 
 const CounterScreen =  () =>{
+
+   const [Counter,setCounter]=useState(0);
+
+
     return (
         <View>
-           <Text>Current Count</Text>
+           <Button 
+             onPress={()=>{setCounter(Counter+1)}}
+             title="Increase"
+             />
+           <Button 
+             onPress={()=>{setCounter(Counter-1)}}
+             title="Decrease"
+             />
+             <Text>Current Count: {Counter}</Text>
         </View>     
            )
 } 
