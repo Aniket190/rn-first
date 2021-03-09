87fb03a6f08d08ea736c0283c95c6a8421013a86
Aniket <aniketkumar190@gmail.com>
2021-03-09 23:00:38 +0530

    ColorScreen random color added

--- src/screens/ColourScreen.js
+++ src/screens/ColourScreen.js
@@ -3,14 +3,22 @@ import {View,Text,StyleSheet,Button} from "react-native";
 
 const ColourScreen = () => {
     return (
-      <View>
+      <View>  
           <Button 
            title="Add a New Color"
           />
+          <View style={{width:100,height:100,backgroundColor: randColor()}}/>
           </View>
     )
 }
 
 const styles=StyleSheet.create({});
+
+const randColor=()=>{
+   const red=Math.floor(Math.random()*255);
+   const green=Math.floor(Math.random()*255);
+   const blue=Math.floor(Math.random()*255);
+   return `rgb(${red},${green},${blue})`
+}
 
 export default ColourScreen;
