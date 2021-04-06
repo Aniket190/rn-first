bac5d3c662a5e812f627583c91b2d61e9c4d8944
Aniket <aniketkumar190@gmail.com>
2021-04-06 13:30:35 +0530

    Square Color complete

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,10 +1,43 @@
 import React,{useState} from 'react';
 import {Text,View,StyleSheet} from 'react-native';
+import ColorComp from './Components/ColorComp'
+
+const COUNTER_VAR=50;
 
 const SquareScreen=()=>{
+
+   const [red,setRed]=useState(0);
+   const [green,setGreen]=useState(0);
+   const [blue,setBlue]=useState(0);
+
+   //Helper Funtion
+    const setColor = (color,change) =>{
+       switch(color)
+        {
+            case 'red':
+                red+change>255 || red+change<0?null:setRed(red+change);
+                return;
+            case 'green':
+                green+change>255 || green+change<0?null:setGreen(green+change);
+                return;
+            case 'blue':
+                blue+change>255 || blue+change<0?null:setBlue(blue+change);
+                return;
+            default:
+                return;
+        }
+    }
+    
+
    return (
        <View>
-           <Text>Square Screen</Text>
+           <ColorComp onIncrease={()=>setColor('red',COUNTER_VAR)} onDecrease={()=>setColor('red',-1*COUNTER_VAR)} color="Red"/>
+           <ColorComp onIncrease={()=>setColor('green',COUNTER_VAR)} onDecrease={()=>setColor('green',-1*COUNTER_VAR)} color="green"/>
+           <ColorComp onIncrease={()=>setColor('blue',COUNTER_VAR)} onDecrease={()=>setColor('blue',-1*COUNTER_VAR)} color="blue"/>
+ 
+           
+           <View style={{height:100,width:100,backgroundColor:`rgb(${red},${green},${blue})`}}/>
+           
        </View>
    )
 }
