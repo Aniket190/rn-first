c52d81183d70d83bd979b19bb684335935062dce
Aniket <aniketkumar190@gmail.com>
2021-04-16 12:25:50 +0530

    Counter screen switched to reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,19 +1,32 @@
-import React,{useState} from "react";
+import React,{useState,useReducer} from "react";
 import {Text,View,StyleSheet,Button} from "react-native"
+
+const reducer = (state,action) =>{
+  switch(action.type)
+  {
+    case 'INCREMENT':
+        return {...state,Counter: state.Counter+action.payload}
+    case 'DECREMENT':
+      return {...state,Counter: state.Counter+action.payload}
+     default:
+        return state;
+
+  }
+}
 
 const CounterScreen =  () =>{
 
-   const [Counter,setCounter]=useState(0);
-
+   const [state,dispatch]=useReducer(reducer,{Counter:0});
+   const {Counter}=state;
 
     return (
         <View>
            <Button 
-             onPress={()=>{setCounter(Counter+1)}}
+             onPress={()=>dispatch({type:'INCREMENT',payload:1})}
              title="Increase"
              />
            <Button 
-             onPress={()=>{setCounter(Counter-1)}}
+             onPress={()=>dispatch({type:'DECREMENT',payload:-1})}
              title="Decrease"
              />
              <Text>Current Count: {Counter}</Text>
