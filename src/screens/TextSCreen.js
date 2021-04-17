6e41e674266097108fbd5b4308004f9c62789e95
Aniket <aniketkumar190@gmail.com>
2021-04-17 12:31:59 +0530

    Text Scrren completes

--- src/screens/TextSCreen.js
+++ src/screens/TextSCreen.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {View,Text,StyleSheet,TextInput} from 'react-native';
 
 const TextScreen =() =>{
+    
+    const [name,setName]= useState('');
+
     return (
         <view>
+            <Text>PASSWORD</Text>
             <TextInput 
                style={styles.input}
                autoCapitalize="none"
                autoCorrect={false}
+               value={name}
+               onChangeText={newValue => setName(newValue)}
                />
+              {name.length < 4 ? <Text>Password must be 4 characters</Text>: null}
+            
+
             </view>
     )
 }
